5d9d6aedc03812e5cb6d13ee96d868f117b8e75d
Manoranga <prabodhamanoranga@gmail.com>
2018-10-07 15:52:30 +0530

    error handle

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 
 const produtsRoutes = require('./api/routes/products');
 
+app.use(morgan('dev'));
+
 app.use('/products',produtsRoutes);
+
+
+app.use((req,res,next)=>{
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error,req,res,next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message : error.message
+        }
+    })
+})
 
 module.exports = app
