13fe7aa29c74b9ef35a3972f8b00d0dd719538ed
Manoranga <prabodhamanoranga@gmail.com>
2018-10-07 23:02:29 +0530

    add new

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
-
+// create routes
 const produtsRoutes = require('./api/routes/products');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+//enable cors
+
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if(req.method ==='OPTION'){
+        res.header("Access-Control-Allow-Methods", 'GET,POST,PUT,DELETE,PATCH');
+        return res.status(200).json({});
+    }
+    next();
+});
+ //set routes
 app.use('/products',produtsRoutes);
 
-
+// error handling
 app.use((req,res,next)=>{
     const error = new Error('Not Found');
     error.status = 404;
