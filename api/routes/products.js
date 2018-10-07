4c20e156d3fd5d915e80383ec18c6cb21b2a4209
Manoranga <prabodhamanoranga@gmail.com>
2018-10-07 10:24:17 +0530

    initial

---
+++ api/routes/products.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req,res,next)=>{
+    res.status(200).json({
+        message : 'HTTP get method works'
+    })
+});
+
+
+router.post('/',(req,res,next)=>{
+    res.status(200).json({
+        message : 'HTTP post method works'
+    })
+});
+ module.exports = router;
---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+
+const produtsRoutes = require('./api/routes/products');
+
+app.use('/products',produtsRoutes);
+
+module.exports = app
