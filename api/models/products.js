6c7460e2b42deb31aa6ba74122eacbaa00a37cbe
Manoranga <prabodhamanoranga@gmail.com>
2018-10-15 23:09:45 +0530

    add validation

--- api/models/products.js
+++ api/models/products.js
@@ -2,8 +2,8 @@ const mongooes = require('mongoose');
 
 const productSchema = mongooes.Schema({
     _id : mongooes.Schema.Types.ObjectId,
-    name : String,
-    price : Number
+    name : { type :String, require},
+    price : {type :Number, require}
 });
 
 module.exports = mongooes.model('Products',productSchema);
