3e72523aefa53d51ce1d3597654127039c1385e4
Manoranga <prabodhamanoranga@gmail.com>
2018-10-16 21:10:55 +0530

    .....

--- api/models/products.js
+++ api/models/products.js
@@ -2,8 +2,8 @@ const mongooes = require('mongoose');
 
 const productSchema = mongooes.Schema({
     _id : mongooes.Schema.Types.ObjectId,
-    name : { type :String, require},
-    price : {type :Number, require}
+    name : { type :String, require : true},
+    price : {type :Number, require : true}
 });
 
 module.exports = mongooes.model('Products',productSchema);
